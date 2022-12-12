7808103233afa6bb35f9ff1df590b9e0f0eab299
sh1r1sha <shirishakudurmalla@gmail.com>
2022-12-12 13:47:40 +0530

    wheatherForecastController Changes

--- Project1/Controllers/WeatherForecastController.cs
+++ Project1/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Project1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Haze"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
